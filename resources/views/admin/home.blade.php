9a031a48bd43b60f9e0eae28b89d9e42c030da50
Matia Mathias <matianyanzi@gmail.com>
2024-11-25 13:21:16 +0300

    modifies form

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,35 +3,68 @@
 <div class="container">
     <br>
     <hr>
-    <h2> <span class="tufaayo-color">Edit</span> content</h2>
-    <br>
-    <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-6">
-            <form method="post" action="{{ route('save.settings') }}">
-                @csrf
-                <div class="row g-3"> 
-                    @foreach($setting as $set)
-                        <div class="col-12">
-                            <div class="row">
-                                <div class="col-md-3">                                    
-                                    <label for="key">{{ $set->key }}</label>
-                                </div>
-                                <div class="col-md-9">
-                                    <input type="text" name="{{ $set->key }}" class="form-control" value="{{ $set->value }}">
-                                    <!-- <textarea class="form-control" placeholder="{{ $set->value }}" style="height: 100px"></textarea> -->
-                                </div>
-                            </div>                       
-                        </div>
-                    @endforeach
-                    <div class="col-12">
-                        <button class="btn btn-primary w-100 py-3" type="submit">Save Changes</button>
-                    </div>
+    <form action="{{ route('save.settings') }}" method="post">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 col-lg-6">
+                <h4>Home Page</h4>
+                <div class="form-group">
+                    <label for="">Home page Background Image</label><br>
+                    <input type="file" name="background_image">
                 </div>
-            </form>
+                
+                <div class="form-group">
+                    <label for="">Title</label><br>
+                    <input type="text" name="title" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">Schools visited</label><br>
+                    <input type="number" name="school_visited" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">Communities visited</label><br>
+                    <input type="number" name="communities_visited" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="">Number of members</label><br>
+                    <input type="number" name="number_of_members" class="form-control">
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-6">
+                <h4>About us Page</h4>
+    
+                <div class="form-group">
+                    <label for="">About us page Background Image</label><br>
+                    <input type="file" name="aboutus_background_image">
+                </div>
+    
+                <div class="formgroup">
+                    <label for="">Our location</label>
+                    <input type="text" name="location" class="form-control">
+                </div>
+                
+                <div class="formgroup">
+                    <label for="">Our phone number</label>
+                    <input type="phone" name="phone_number" class="form-control">
+                </div>
+                
+                <div class="formgroup">
+                    <label for="">Our Email address</label>
+                    <input type="phone" name="email_address" class="form-control">
+                </div>
+    
+                <div class="formgroup">
+                    <label for="">
+                        Why you should trust us
+                    </label>
+                    <textarea name="why_trust_us" class="form-control"></textarea>
+                </div>
+            </div>
+    
+            <div class="col-sm-12 col-md-12 col-sm-12">
+                <br>
+                <button class="btn btn-primary w-100 py-3" type="submit">Save Changes</button>
+            </div>
         </div>
-        <div class="col-sm-12 col-md-6 col-lg-6">
-
-        </div>
-    </div>
+    </form>
 </div>
 @endsection('content')
